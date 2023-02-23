3eb170ea870a3ec6edc88e8966ca46b6c8be978f
Aline Cristina Viana da Costa <alinecristinaviana@gmail.com>
2023-02-23 19:53:56 -0300

    add hooks

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ThemeProvider } from 'styled-components';
-import GlobalStyles from './styles/global';
 import theme from './styles/theme';
+import GlobalStyles from './styles/global';
+import { AuthProvider } from './hooks/auth';
 import { Routes } from './routes';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <GlobalStyles/>
-      <Routes />
+      <GlobalStyles />
+        <AuthProvider>
+          <Routes />
+        </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -3,8 +3,19 @@ import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
 import { FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/auth';
+import { useState } from 'react';
 
 export function SignIn() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const { signIn } = useAuth();
+
+    function handleSignIn() {
+        signIn({ email, password })
+    }
+
     return(
         <Container>
             <Form>
@@ -17,15 +28,17 @@ export function SignIn() {
                     placeholder="E-mail"
                     type="text"
                     icon={FiMail}
+                    onChange={event => setEmail(event.target.value)}
                 />
                 
                 <Input 
                     placeholder="Senha"
                     type="password"
                     icon={FiLock}
+                    onChange={event => setPassword(event.target.value)}
                 />
 
-                <Button title="Entrar" />
+                <Button title="Entrar" onClick={handleSignIn} />
                     
                 <Link to="/register">
                     Criar Conta
